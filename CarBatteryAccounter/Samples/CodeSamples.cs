079a50077394687c60b07052acf8e03f1c44c865
Vorobushek221 <vorobushek221@gmail.com>
2017-03-27 13:35:01 +0300

    Add project files.

---
+++ CarBatteryAccounter/Samples/CodeSamples.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+
+namespace CarBatteryAccounter.Samples
+{
+    public static class CodeSamples
+    {
+        public static void DbConnectionTest()
+        {
+            OdbcConnection connection = new OdbcConnection("Driver={Microsoft dBASE Driver (*.dbf)};DriverID=277;Dbq=d:\\db;");
+            String SQL = "SELECT * FROM Sales.dbf";
+            connection.Open();
+
+            OdbcCommand MyCommand = new OdbcCommand(SQL, connection);
+            OdbcDataReader dr = MyCommand.ExecuteReader();
+            while (dr.Read())
+            {
+                var a = dr["Cust_id"].ToString();
+                var b = dr.GetInt32(1);
+
+            }
+        }
+
+    }
+}
